afb3065b20bb4260330bad247f27e3ba692d18f5
Shalini Lodhi <55933789+Shalini-lodhi@users.noreply.github.com>
2022-04-07 12:38:55 +0530

    Time: 45 ms (65.32%), Space: 27.6 MB (12.69%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,17 +1,5 @@
 class Solution {
 public:
-    // int search(vector<int>& nums, int target) {
-    //     int sz=sizeof(nums)/sizeof(nums[0]);
-    //     if(sz==0 && nums[0]==target) return 0;
-    //     int start=0, end=sz-1; int mid;
-    //     while(start<=end){
-    //         mid = start +(end-start)/2;
-    //         if(nums[mid]==target) return mid;
-    //         else if(target<nums[mid]) end=mid-1;
-    //         else start=mid+1;
-    //     }
-    //     return -1;
-    // }
     int search(vector<int>& arr,int k){
         int n=arr.size();
         int start=0, end=n-1;
