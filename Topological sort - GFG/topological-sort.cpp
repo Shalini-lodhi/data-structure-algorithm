2d1a899157af41ed6b877ac1cce07786400690d8
Shalini Lodhi <55933789+Shalini-lodhi@users.noreply.github.com>
2022-06-22 17:24:21 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -7,33 +7,63 @@ class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order. 
-	void topoSort(int node, vector<bool>& visited, stack<int>& s, vector<int> adj[]){
-        visited[node]=true;
+// 	void topoSort(int node, vector<bool>& visited, stack<int>& s, vector<int> adj[]){
+//         visited[node]=true;
         
-        for(auto neighbour: adj[node]){
-            if(!visited[neighbour])
-                topoSort(neighbour, visited, s, adj);
-        }
+//         for(auto neighbour: adj[node]){
+//             if(!visited[neighbour])
+//                 topoSort(neighbour, visited, s, adj);
+//         }
         
-        s.push(node);
-	}
-	vector<int> topoSort(int V, vector<int> adj[]) 
-	{
-	    stack<int> s;
-	    vector<bool> visited(V);
-	    for(int i=0; i<V; i++){
-	        if(!visited[i]){
-	            topoSort(i, visited, s, adj);
-	        }
-	    }
+//         s.push(node);
+// 	}
+// 	vector<int> topoSort(int V, vector<int> adj[]) 
+// 	{
+// 	    stack<int> s;
+// 	    vector<bool> visited(V);
+// 	    for(int i=0; i<V; i++){
+// 	        if(!visited[i]){
+// 	            topoSort(i, visited, s, adj);
+// 	        }
+// 	    }
 	    
-	    vector<int> ans;
-	    while(!s.empty()){
-	        ans.push_back(s.top());
-	        s.pop();
-	    }
-	    return ans;
-	}
+// 	    vector<int> ans;
+// 	    while(!s.empty()){
+// 	        ans.push_back(s.top());
+// 	        s.pop();
+// 	    }
+// 	    return ans;
+// 	}
+    vector<int> topoSort(int V, vector<int> adj[]) 
+{    //find all inDegree of vertices
+    vector<int> inDegree(V);
+    for(int i=0;i<V;i++){
+        for(auto j: adj[i]){
+            inDegree[j]++;
+        }
+    }
+    //pushinf all inDegree 0 to queue
+    queue<int> q;
+    for(int i=0;i<V;i++){
+        if(inDegree[i]==0) q.push(i);
+    }
+    //do bfs
+    
+    vector<int> ans;
+    while(!q.empty()){
+        int front = q.front();
+        q.pop();
+        
+        //ans store
+        ans.push_back(front);
+        
+        //neighbour inDegree update
+        for(auto neighbour: adj[front]){
+            inDegree[neighbour]--;
+            if(inDegree[neighbour]==0) q.push(neighbour);
+        }
+    }
+    return ans;}
 };
 
 // { Driver Code Starts.
