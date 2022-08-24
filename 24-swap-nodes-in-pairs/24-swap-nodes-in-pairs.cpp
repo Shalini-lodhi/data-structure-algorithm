c755b39e7f8be564b56c1ad347997712db2bc9eb
Shalini Lodhi <55933789+Shalini-lodhi@users.noreply.github.com>
2022-08-24 17:14:09 +0530

    Time: 3 ms (73.60%), Space: 7.6 MB (54.82%) - LeetHub

---
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+     ListNode* reverseKGroup(ListNode* head, int k) {
+        //base case
+        ListNode* kCount = head;
+        for(int i=0; i< k; i++){
+            if(kCount == NULL) return head;
+            kCount = kCount -> next;
+        }        
+        //step1: reverse first k ListNode
+        ListNode* next = NULL;
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        int count = 0;
+        
+        while(curr != NULL && count < k){
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+            count ++;
+        }
+        
+        //step2: recursion call
+        head -> next = reverseKGroup(next, k);
+        
+        //step3: return head
+        return prev;
+    }
+    ListNode* swapPairs(ListNode* head) {
+        int k =2;
+        return reverseKGroup(head, k);
+    }
+};
