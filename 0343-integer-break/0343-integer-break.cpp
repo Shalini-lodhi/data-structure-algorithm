4180cc860d2631860e2192ad67dd2a531f8b3393
Shalini Lodhi <55933789+Shalini-lodhi@users.noreply.github.com>
2025-03-05 01:27:31 +0530

    Time: 7 ms (4.29%), Space: 8.7 MB (30.29%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,30 +1,25 @@
+using namespace std;
+
 class Solution {
 public:
     int integerBreak(int n) {
-        // vector<int> dp(n + 1, 0);
-        // dp[0] = 0; dp[1] = 1;
-
-        // for(int i = 2; i <= n ; ++i){
-        //     for(int j = i; j <)
-        // }
-
-        // return dp[n];
         if (n == 2)
             return 1;
         if (n == 3)
             return 2;
 
-        int product = 1;
+        vector<int> dp(n + 1, 0);
+        dp[2] = 1;
+        dp[3] = 2;
 
-        // Break as many 3s as possible
-        while (n > 4) {
-            product *= 3;
-            n -= 3;
+        for (int i = 4; i <= n; i++) {
+            for (int j = 1; j <= i / 2; j++) {
+                dp[i] = max(dp[i], max(j * (i - j), j * dp[i - j]));
+            }
         }
-
-        // Multiply the last remaining number (either 2 or 4)
-        product *= n;
-
-        return product;
+        for (int i = 0; i <= n; i++) {
+            cout << i << "-> " << dp[i] << endl;
+        }
+        return dp[n];
     }
 };
